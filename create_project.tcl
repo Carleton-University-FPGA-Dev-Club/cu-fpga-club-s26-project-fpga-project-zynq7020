0b0743d0f38b550a1736512291a6cc5c4b5917fa
Sebastian Petelle <sebastianpetelle@cmail.carleton.ca>
2026-07-16 23:15:52 -0400

    Restructure to fit with modular design philosphy

--- create_project.tcl
+++ create_project.tcl
@@ -3,11 +3,12 @@ create_project zybo_imgprocP1 ./phase1 -part xc7z020clg400-1 -force
 # can probably combine this with files.f so we dont have to update both.
 add_files [list \
     hw/rtl/pkg/img_pipe_pkg.sv
-    hw/rtl/source/average_color.sv
-    hw/rtl/source/pipeline_top.sv
-    hw/rtl/source/test_image_gen.sv
-    hw/rtl/source/timing_gen.sv
-    hw/rtl/source/rgb_led_driver.sv
+    hw/rtl/proc/average_color.sv
+    hw/rtl/proc/invert_colour.sv
+    hw/rtl/pipeline_top.sv
+    hw/rtl/gen/test_image_gen.sv
+    hw/rtl/gen/timing_gen.sv
+    hw/rtl/disp/rgb_led_driver.sv
     hw/rtl/top.sv ]
 add_files -fileset sim_1 hw\sim\tb_pipeline.sv
 add_files -fileset constrs_1 Zybo-Z7-Master.xdc
