91acdf59e3497af9c3ec2ff4acefe4c402d833a9
Sebastian Petelle <sebastianpetelle@cmail.carleton.ca>
2026-07-16 23:15:52 -0400

    Restructure to fit with modular design philosphy

--- files.f
+++ files.f
@@ -1,7 +1,7 @@
 hw/rtl/pkg/img_pipe_pkg.sv
-hw/rtl/source/average_color.sv
-hw/rtl/source/invert_colour.sv
-hw/rtl/source/pipeline_top.sv
-hw/rtl/source/test_image_gen.sv
-hw/rtl/source/timing_gen.sv
-hw/rtl/source/rgb_led_driver.sv
+hw/rtl/proc/average_color.sv
+hw/rtl/proc/invert_colour.sv
+hw/rtl/pipeline_top.sv
+hw/rtl/gen/test_image_gen.sv
+hw/rtl/gen/timing_gen.sv
+hw/rtl/disp/rgb_led_driver.sv
